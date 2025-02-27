e18f43674afaf4859a374b1257a120d906ee0771
Cichy <srej100@gmail.com>
2025-02-27 16:23:40 +0100

    index changes

--- php/index.php
+++ php/index.php
@@ -6,7 +6,7 @@
     <title>Home - Index</title>
 </head>
 <body>
-    <header></head><h1>Welcome to the Index Page</h1></header>
+    <header></head><h1>Welcome to the (Vulnerable) Index Page</h1></header>
     <main>
         <ul>
             <li><a href="./comments/index.php">Comments</a></li>
